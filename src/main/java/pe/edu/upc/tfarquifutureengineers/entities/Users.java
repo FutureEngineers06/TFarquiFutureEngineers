559e619ee87358aacab45b6a6bd786453b2cbad0
Kacchan368 <angelochipulina@gmail.com>
2023-05-24 10:52:38 -0500

    Users entities

--- src/main/java/pe/edu/upc/tfarquifutureengineers/entities/Users.java
+++ src/main/java/pe/edu/upc/tfarquifutureengineers/entities/Users.java
@@ -1,10 +1,63 @@
 package pe.edu.upc.tfarquifutureengineers.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "Users")
 public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idUsers;
+    @Column(name = "rol", length = 20,nullable = false)
+    private String rol;
+    @Column(name = "nombre_completo", length = 100,nullable = false)
+    private String nombre_completo;
+    @Column(name = "correo_electronico", length = 100,nullable = false)
+    private String correo_electronico;
+    @Column(name = "contraseña", length = 150,nullable = false)
+    private String contraseña;
 
+    public Users() {
+
+    }
+
+    public int getIdUsers() {
+        return idUsers;
+    }
+
+    public void setIdUsers(int idUsers) {
+        this.idUsers = idUsers;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public String getNombre_completo() {
+        return nombre_completo;
+    }
+
+    public void setNombre_completo(String nombre_completo) {
+        this.nombre_completo = nombre_completo;
+    }
+
+    public String getCorreo_electronico() {
+        return correo_electronico;
+    }
+
+    public void setCorreo_electronico(String correo_electronico) {
+        this.correo_electronico = correo_electronico;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
 }
