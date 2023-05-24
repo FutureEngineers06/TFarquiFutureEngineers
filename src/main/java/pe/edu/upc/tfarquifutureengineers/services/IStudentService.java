c48aae0c394a09e08e4d34dc908849bb447a60f3
Romina-Stephanie-Huamani-Felix <118565562+Romina-Stephanie-Huamani-Felix@users.noreply.github.com>
2023-05-24 17:27:10 -0500

    update student service

--- src/main/java/pe/edu/upc/tfarquifutureengineers/services/IStudentService.java
+++ src/main/java/pe/edu/upc/tfarquifutureengineers/services/IStudentService.java
@@ -1,4 +1,10 @@
 package pe.edu.upc.tfarquifutureengineers.services;
 
+import pe.edu.upc.tfarquifutureengineers.entities.Student;
+
+import java.util.List;
+
 public interface IStudentService {
+    public void insert(Student student);
+    List<Student> list();
 }
