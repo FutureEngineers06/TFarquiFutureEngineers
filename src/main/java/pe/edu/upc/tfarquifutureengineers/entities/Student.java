a34690d85427d0efc2b78b81248c574d67a27842
Romina-Stephanie-Huamani-Felix <118565562+Romina-Stephanie-Huamani-Felix@users.noreply.github.com>
2023-05-24 16:06:25 -0500

    generate constructors

--- src/main/java/pe/edu/upc/tfarquifutureengineers/entities/Student.java
+++ src/main/java/pe/edu/upc/tfarquifutureengineers/entities/Student.java
@@ -7,4 +7,16 @@ public class Student {
     private int users_user_id;
     private int membresia_id;
     private int sala_id;
+
+    public Student() {
+    }
+
+    public Student(int idStudent, String colegio, int edad, int users_user_id, int membresia_id, int sala_id) {
+        this.idStudent = idStudent;
+        this.colegio = colegio;
+        this.edad = edad;
+        this.users_user_id = users_user_id;
+        this.membresia_id = membresia_id;
+        this.sala_id = sala_id;
+    }
 }
