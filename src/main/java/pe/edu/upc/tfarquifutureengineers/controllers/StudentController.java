6a204c5461412e8b0d6318b4efcf1cbafea181f9
Romina-Stephanie-Huamani-Felix <118565562+Romina-Stephanie-Huamani-Felix@users.noreply.github.com>
2023-05-24 18:28:23 -0500

    update student controller -insert and list methods-

--- src/main/java/pe/edu/upc/tfarquifutureengineers/controllers/StudentController.java
+++ src/main/java/pe/edu/upc/tfarquifutureengineers/controllers/StudentController.java
@@ -1,4 +1,31 @@
 package pe.edu.upc.tfarquifutureengineers.controllers;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.upc.tfarquifutureengineers.dtos.StudentDTO;
+import pe.edu.upc.tfarquifutureengineers.entities.Student;
+import pe.edu.upc.tfarquifutureengineers.services.IStudentService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/students")
 public class StudentController {
+    @Autowired
+    private IStudentService sS;
+@PostMapping
+    public void insert(@RequestBody StudentDTO dto){
+    ModelMapper m = new ModelMapper();
+    Student s = m.map(dto,Student.class);
+    sS.insert(s);
+    }
+@GetMapping
+    public List<StudentDTO> list(){
+    return sS.list().stream().map(x->{
+        ModelMapper m = new ModelMapper();
+        return m.map(x,StudentDTO.class);
+    }).collect(Collectors.toList());
+    }
 }
