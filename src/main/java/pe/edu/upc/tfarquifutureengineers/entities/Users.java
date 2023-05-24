abe00106e51a8255db350f66528a454f8a9f6ae5
Kacchan368 <angelochipulina@gmail.com>
2023-05-24 10:45:36 -0500

    update

--- src/main/java/pe/edu/upc/tfarquifutureengineers/entities/Users.java
+++ src/main/java/pe/edu/upc/tfarquifutureengineers/entities/Users.java
@@ -1,5 +1,10 @@
 package pe.edu.upc.tfarquifutureengineers.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Users")
 public class Users {
 
 }
--- src/main/java/pe/edu/upc/tfarquifutureengineers/repositories/IUsersRepository.java
+++ src/main/java/pe/edu/upc/tfarquifutureengineers/repositories/IUsersRepository.java
@@ -1,4 +1,7 @@
 package pe.edu.upc.tfarquifutureengineers.repositories;
 
-public interface IUsersRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import pe.edu.upc.tfarquifutureengineers.entities.Users;
+
+public interface IUsersRepository extends JpaRepository<Users,Integer> {
 }
