12cc1930b6b0ba71f33617ec40aed25e5e694771
Romina-Stephanie-Huamani-Felix <118565562+Romina-Stephanie-Huamani-Felix@users.noreply.github.com>
2023-05-24 16:09:32 -0500

    getters and setters class student

--- src/main/java/pe/edu/upc/tfarquifutureengineers/entities/Student.java
+++ src/main/java/pe/edu/upc/tfarquifutureengineers/entities/Student.java
@@ -19,4 +19,52 @@ public class Student {
         this.membresia_id = membresia_id;
         this.sala_id = sala_id;
     }
+
+    public int getIdStudent() {
+        return idStudent;
+    }
+
+    public void setIdStudent(int idStudent) {
+        this.idStudent = idStudent;
+    }
+
+    public String getColegio() {
+        return colegio;
+    }
+
+    public void setColegio(String colegio) {
+        this.colegio = colegio;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getUsers_user_id() {
+        return users_user_id;
+    }
+
+    public void setUsers_user_id(int users_user_id) {
+        this.users_user_id = users_user_id;
+    }
+
+    public int getMembresia_id() {
+        return membresia_id;
+    }
+
+    public void setMembresia_id(int membresia_id) {
+        this.membresia_id = membresia_id;
+    }
+
+    public int getSala_id() {
+        return sala_id;
+    }
+
+    public void setSala_id(int sala_id) {
+        this.sala_id = sala_id;
+    }
 }
