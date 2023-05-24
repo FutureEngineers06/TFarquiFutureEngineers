f6b0b81b54a716cbd66c3f010e71ddc0ba59c8d6
Romina-Stephanie-Huamani-Felix <118565562+Romina-Stephanie-Huamani-Felix@users.noreply.github.com>
2023-05-24 16:02:42 -0500

    update class student

--- src/main/java/pe/edu/upc/tfarquifutureengineers/entities/Student.java
+++ src/main/java/pe/edu/upc/tfarquifutureengineers/entities/Student.java
@@ -1,4 +1,10 @@
 package pe.edu.upc.tfarquifutureengineers.entities;
 
 public class Student {
+    private int idStudent;
+    private String colegio;
+    private int edad;
+    private int users_user_id;
+    private int membresia_id;
+    private int sala_id;
 }
