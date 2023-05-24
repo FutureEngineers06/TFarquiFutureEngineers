aa6c0952c6f77625f4294fb927946b3b746855c1
Romina-Stephanie-Huamani-Felix <118565562+Romina-Stephanie-Huamani-Felix@users.noreply.github.com>
2023-05-24 17:14:01 -0500

    update student repository

--- src/main/java/pe/edu/upc/tfarquifutureengineers/repositories/IStudentRepository.java
+++ src/main/java/pe/edu/upc/tfarquifutureengineers/repositories/IStudentRepository.java
@@ -1,4 +1,9 @@
 package pe.edu.upc.tfarquifutureengineers.repositories;
 
-public interface IStudentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pe.edu.upc.tfarquifutureengineers.entities.Student;
+
+@Repository
+public interface IStudentRepository extends JpaRepository<Student,Integer> {
 }
