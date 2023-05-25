1012d19de4b28fad7eafa57786ef2610dc0e36b1
Kacchan368 <angelochipulina@gmail.com>
2023-05-24 19:09:10 -0500

    Service, Controller, implements y DTO

--- src/main/java/pe/edu/upc/tfarquifutureengineers/services/IUsersService.java
+++ src/main/java/pe/edu/upc/tfarquifutureengineers/services/IUsersService.java
@@ -1,4 +1,11 @@
 package pe.edu.upc.tfarquifutureengineers.services;
 
+import pe.edu.upc.tfarquifutureengineers.entities.Users;
+
+import java.util.List;
+
 public interface IUsersService {
+
+    public void insert(Users users);
+    List<Users> list();
 }
