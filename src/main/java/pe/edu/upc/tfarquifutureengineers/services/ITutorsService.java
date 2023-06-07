1cdeaefa4efc200e893a982e2908fe2d2956eb61
Kacchan368 <angelochipulina@gmail.com>
2023-06-07 17:04:47 -0500

    update tutor

--- src/main/java/pe/edu/upc/tfarquifutureengineers/services/ITutorsService.java
+++ src/main/java/pe/edu/upc/tfarquifutureengineers/services/ITutorsService.java
@@ -1,4 +1,13 @@
 package pe.edu.upc.tfarquifutureengineers.services;
 
+import pe.edu.upc.tfarquifutureengineers.entities.Student;
+import pe.edu.upc.tfarquifutureengineers.entities.Tutors;
+
+import java.util.List;
+
 public interface ITutorsService {
+    public void insert(Tutors tutors);
+    List<Student> list();
+    public void delete(int idTutor);
+    public Student listId(int idTutor);
 }
--- src/main/java/pe/edu/upc/tfarquifutureengineers/servicesimplement/TutorsServiceImplement.java
+++ src/main/java/pe/edu/upc/tfarquifutureengineers/servicesimplement/TutorsServiceImplement.java
@@ -1,4 +1,29 @@
 package pe.edu.upc.tfarquifutureengineers.serviceimplement;
 
-public class TutorsServiceImplement {
+import pe.edu.upc.tfarquifutureengineers.entities.Student;
+import pe.edu.upc.tfarquifutureengineers.entities.Tutors;
+import pe.edu.upc.tfarquifutureengineers.services.ITutorsService;
+
+import java.util.List;
+
+public class TutorsServiceImplement implements ITutorsService {
+    @Override
+    public void insert(Tutors tutors) {
+        
+    }
+
+    @Override
+    public List<Student> list() {
+        return null;
+    }
+
+    @Override
+    public void delete(int idTutor) {
+
+    }
+
+    @Override
+    public Student listId(int idTutor) {
+        return null;
+    }
 }
