9da764ffb5e69658a143b0429e543b167488dc73
Kacchan368 <angelochipulina@gmail.com>
2023-06-07 17:52:35 -0500

    Update tutors

--- src/main/java/pe/edu/upc/tfarquifutureengineers/services/ITutorsService.java
+++ src/main/java/pe/edu/upc/tfarquifutureengineers/services/ITutorsService.java
@@ -1,13 +1,12 @@
 package pe.edu.upc.tfarquifutureengineers.services;
 
-import pe.edu.upc.tfarquifutureengineers.entities.Student;
 import pe.edu.upc.tfarquifutureengineers.entities.Tutors;
 
 import java.util.List;
 
 public interface ITutorsService {
     public void insert(Tutors tutors);
-    List<Student> list();
+    List<Tutors> list();
     public void delete(int idTutor);
-    public Student listId(int idTutor);
+        public Tutors listId(int idTutor);
 }
--- src/main/java/pe/edu/upc/tfarquifutureengineers/servicesimplement/TutorsServiceImplement.java
+++ src/main/java/pe/edu/upc/tfarquifutureengineers/servicesimplement/TutorsServiceImplement.java
@@ -1,29 +1,35 @@
 package pe.edu.upc.tfarquifutureengineers.serviceimplement;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import pe.edu.upc.tfarquifutureengineers.entities.Student;
 import pe.edu.upc.tfarquifutureengineers.entities.Tutors;
+import pe.edu.upc.tfarquifutureengineers.repositories.ITutorsRepository;
 import pe.edu.upc.tfarquifutureengineers.services.ITutorsService;
 
 import java.util.List;
 
 public class TutorsServiceImplement implements ITutorsService {
+
+    @Autowired
+    private ITutorsRepository tR;
+
     @Override
     public void insert(Tutors tutors) {
-        
+        tR.save(tutors);
     }
 
     @Override
-    public List<Student> list() {
-        return null;
+    public List<Tutors> list() {
+        return tR.findAll();
     }
 
     @Override
     public void delete(int idTutor) {
-
+        tR.deleteById(idTutor);
     }
 
     @Override
-    public Student listId(int idTutor) {
-        return null;
+    public Tutors listId(int idTutor) {
+        return tR.findById(idTutor).orElse(new Tutors());
     }
 }
