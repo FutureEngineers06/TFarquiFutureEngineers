666f2f715058b62318884580d6ff6382ceabb28f
Kacchan368 <angelochipulina@gmail.com>
2023-06-07 18:52:26 -0500

    update

--- src/main/java/pe/edu/upc/tfarquifutureengineers/servicesimplement/TutorsServiceImplement.java
+++ src/main/java/pe/edu/upc/tfarquifutureengineers/servicesimplement/TutorsServiceImplement.java
@@ -1,13 +1,14 @@
 package pe.edu.upc.tfarquifutureengineers.serviceimplement;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import pe.edu.upc.tfarquifutureengineers.entities.Student;
 import pe.edu.upc.tfarquifutureengineers.entities.Tutors;
 import pe.edu.upc.tfarquifutureengineers.repositories.ITutorsRepository;
 import pe.edu.upc.tfarquifutureengineers.services.ITutorsService;
 
 import java.util.List;
-
+@Service
 public class TutorsServiceImplement implements ITutorsService {
 
     @Autowired
