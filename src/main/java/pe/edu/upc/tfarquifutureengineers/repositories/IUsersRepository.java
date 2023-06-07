84870b59ff3fa08cee06b5218dec328479c5bc82
Kacchan368 <angelochipulina@gmail.com>
2023-06-07 18:43:49 -0500

    actua

--- src/main/java/pe/edu/upc/tfarquifutureengineers/repositories/IUsersRepository.java
+++ src/main/java/pe/edu/upc/tfarquifutureengineers/repositories/IUsersRepository.java
@@ -1,7 +1,9 @@
 package pe.edu.upc.tfarquifutureengineers.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pe.edu.upc.tfarquifutureengineers.entities.Users;
 
+@Repository
 public interface IUsersRepository extends JpaRepository<Users,Integer> {
 }
