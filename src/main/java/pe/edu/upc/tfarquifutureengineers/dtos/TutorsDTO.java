49d1a9955e15463ed13659e7d76afea46e7509d6
TadashiBD <tadashidulantobuitron@gmail.com>
2023-06-16 02:12:07 -0500

    Tutors back end Implementado- Debe de haber un User para insertar un Tutor
    {
      "especializacion": "Quimico",
      "idTutors": 0,
      "users_user_id": {
        "contraseña": "tadashi",
        "correo_electronico": "Tadashi@gmail.com",
        "idUsers": 2,
        "nombre_completo": "Tadashi",
        "rol": "Tutor"
      }
    }

--- src/main/java/pe/edu/upc/tfarquifutureengineers/dtos/TutorsDTO.java
+++ src/main/java/pe/edu/upc/tfarquifutureengineers/dtos/TutorsDTO.java
@@ -1,9 +1,12 @@
 package pe.edu.upc.tfarquifutureengineers.dtos;
 
+import pe.edu.upc.tfarquifutureengineers.entities.Users;
+
 public class TutorsDTO {
 
     private int idTutors;
     private String especializacion;
+    private Users Users_user_id;//FOREING KEY de USERS
 
     public int getIdTutors() {
         return idTutors;
@@ -21,5 +24,11 @@ public class TutorsDTO {
         this.especializacion = especializacion;
     }
 
+    public Users getUsers_user_id() {
+        return Users_user_id;
+    }
 
+    public void setUsers_user_id(Users users_user_id) {
+        Users_user_id = users_user_id;
+    }
 }
--- src/main/java/pe/edu/upc/tfarquifutureengineers/entities/Tutors.java
+++ src/main/java/pe/edu/upc/tfarquifutureengineers/entities/Tutors.java
@@ -12,13 +12,26 @@ public class Tutors {
     @Column(name = "especializacion",length = 60,nullable = false)
     private String especializacion;
 
+    @ManyToOne
+    @JoinColumn(name = "id_users")
+    private Users Users_user_id;//FOREING KEY de USERS
+
     public Tutors() {
 
     }
 
-    public Tutors(int idTutor, String especializacion) {
+    public Tutors(int idTutor, String especializacion, Users users_user_id) {
         this.idTutor = idTutor;
         this.especializacion = especializacion;
+        Users_user_id = users_user_id;
+    }
+
+    public Users getUsers_user_id() {
+        return Users_user_id;
+    }
+
+    public void setUsers_user_id(Users users_user_id) {
+        Users_user_id = users_user_id;
     }
 
     public int getIdTutor() {
